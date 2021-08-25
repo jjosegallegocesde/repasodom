725cfdb4aba6096f27dc27cd70f3ce7ff543a257
jjosegallegocesde <61057518+jjosegallegocesde@users.noreply.github.com>
2021-08-25 08:43:41 -0500

    se crean referencias del dom

--- controlador.js
+++ controlador.js
@@ -1,1 +1,14 @@
-console.log("hola pelados");
+let boton=document.getElementById("boton");
+let foto1=document.getElementById("foto1");
+let titulo1=document.getElementById("titulo1");
+let audio1=document.getElementById("audio1");
+
+boton.addEventListener("click",perroGuardian);
+
+function perroGuardian(){
+
+    titulo1.textContent="the scientist";
+    foto1.src="img/foto4.jpg";
+    audio1.src="audio/cancion4.mp3";
+    
+}
